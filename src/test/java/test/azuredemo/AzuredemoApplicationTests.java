fad77def92533d48450d4e797eb4780c5a9556d5
florin.tansanu <florin.tansanu@gmail.com>
2022-02-02 13:59:26 +0200

    Initial commit

---
+++ src/test/java/test/azuredemo/AzuredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package test.azuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
